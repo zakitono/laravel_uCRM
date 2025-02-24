015bdbe279d86ed7c972ec3f04b2526f14e3c51f
zakitono <coppercraft1313@gmail.com>
2025-02-24 18:11:31 +0900

    顧客管理画面　顧客検索後、顧客の登録

---
+++ app/Http/Controllers/PurchaseController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePurchaseRequest;
+use App\Http\Requests\UpdatePurchaseRequest;
+use App\Models\Purchase;
+use App\Models\Customer;
+use App\Models\Item;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\DB;
+
+class PurchaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+//        $customers = Customer::select('id', 'name', 'kana')->get();
+        $items = Item::select('id', 'name', 'price')->
+        where('is_selling', true)->get();
+
+        return Inertia::render('Purchases/Create', [
+//            'customers' => $customers,
+            'items' => $items
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePurchaseRequest $request)
+    {
+        DB::beginTransaction();
+
+        try{
+              $purchase = Purchase::create([
+                  'customer_id' => $request->customer_id,
+                  'status' => $request->status,
+              ]);
+
+              foreach($request->items as $item){
+                  $purchase->items()->attach($purchase->id,[
+                      'item_id' => $item['id'],
+                      'quantity' => $item['quantity'],
+                  ]);
+              }
+
+              DB::commit();
+              return to_route('dashboard');
+
+        }catch (\Exception $e){
+            DB::rollBack();
+            }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Purchase $purchase)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Purchase $purchase)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePurchaseRequest $request, Purchase $purchase)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Purchase $purchase)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_02_23_071008_create_item_purchase_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item_purchase', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->constrained()->onUpdate('cascade');
+            $table->foreignId('purchase_id')->constrained()->onUpdate('cascade');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item_purchase');
+    }
+};
